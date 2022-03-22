ad96135a48c9a94c8c8f0cdd47964ddd7f2103ff
rmznkrts74 <92362712+rmznkrts74@users.noreply.github.com>
2022-03-22 15:03:01 +0300

    Add files via upload

---
+++ VendingMachine.py
@@ -0,0 +1,255 @@
+print("Please enter the cod which one do you want:")
+print("İf you buy to anything else please press 11 after buy to first item")
+print("1-icecek1")
+icecek1 = 1
+print("2-icecek2")
+icecek2 = 2
+print("3-icecek3")
+icecek3 = 3
+print("4-icecek4")
+icecek4 = 2.5
+print("5-icecek5")
+icecek5 = 2.25
+print("6-icecek6")
+icecek6 = 1.5
+print("7-icecek7")
+icecek7 = 1.8
+print("8-icecek8")
+icecek8 = 1.9
+print("9-icecek9")
+icecek9 = 2
+print("10-icecek10")
+icecek10 = 3
+while True:
+    i = eval(input("Please enter the code 1-10:"))
+    number = eval(input("Please enter the how much do you want:"))
+    while True:
+        if i == 1:
+            total = icecek1 * number
+            print("Your total debt: {} ₺ ".format(total))
+            print("Please put the money in the box")
+            print("Your money calculated")
+            money = eval(input("please enter the amount of money:"))
+            if money > total:
+                change = money - total
+                print("Please take the your change in the box: {}".format(change))
+            elif money == total:
+                print("process sucessfull")
+            else:
+                print("Your money not enough please put {} ₺".format(total-money))
+                addMoney = eval(input("Please enter the money which is add :"))
+                print("Please put the money in the box")
+                print("Your money calculated")
+                newTotal = addMoney + money
+                if newTotal > total:
+                    print("Please take your change in the box  {}".format(newTotal-total))
+                elif newTotal == total:
+                    print("Process Sucesfull")
+                    break
+        if i == 2:
+            total = icecek2 * number
+            print("Your total debt: {} ₺ ".format(total))
+            print("Please put the money in the box")
+            print("Your money calculated")
+            money = eval(input("please enter the amount of money:"))
+            if money > total:
+                change = money - total
+                print("Please take the your change in the box: {}".format(change))
+            elif money == total:
+                print("process sucessfull")
+            else:
+                print("Your money not enough please put {} ₺".format(total-money))
+                addMoney = eval(input("Please enter the money which is add :"))
+                print("Please put the money in the box")
+                print("Your money calculated")
+                newTotal = addMoney + money
+                if newTotal > total:
+                    print("Please take your change in the box  {}".format(newTotal-total))
+                elif newTotal == total:
+                    print("Process Sucesfull")
+                    break
+        if i == 3:
+            total = icecek1 * number
+            print("Your total debt: {} ₺ ".format(total))
+            print("Please put the money in the box")
+            print("Your money calculated")
+            money = eval(input("please enter the amount of money:"))
+            if money > total:
+                change = money - total
+                print("Please take the your change in the box: {}".format(change))
+            elif money == total:
+                print("process sucessfull")
+            else:
+                print("Your money not enough please put {} ₺".format(total-money))
+                addMoney = eval(input("Please enter the money which is add :"))
+                print("Please put the money in the box")
+                print("Your money calculated")
+                newTotal = addMoney + money
+                if newTotal > total:
+                    print("Please take your change in the box  {}".format(newTotal-total))
+                elif newTotal == total:
+                    print("Process Sucesfull")
+                    break
+        if i == 4:
+            total = icecek4 * number
+            print("Your total debt: {} ₺ ".format(total))
+            print("Please put the money in the box")
+            print("Your money calculated")
+            money = eval(input("please enter the amount of money:"))
+            if money > total:
+                change = money - total
+                print("Please take the your change in the box: {}".format(change))
+            elif money == total:
+                print("process sucessfull")
+            else:
+                print("Your money not enough please put {} ₺".format(total-money))
+                addMoney = eval(input("Please enter the money which is add :"))
+                print("Please put the money in the box")
+                print("Your money calculated")
+                newTotal = addMoney + money
+                if newTotal > total:
+                    print("Please take your change in the box  {}".format(newTotal-total))
+                elif newTotal == total:
+                    print("Process Sucesfull")
+                    break
+        if i == 5:
+            total = icecek5 * number
+            print("Your total debt: {} ₺ ".format(total))
+            print("Please put the money in the box")
+            print("Your money calculated")
+            money = eval(input("please enter the amount of money:"))
+            if money > total:
+                change = money - total
+                print("Please take the your change in the box: {}".format(change))
+            elif money == total:
+                print("process sucessfull")
+            else:
+                print("Your money not enough please put {} ₺".format(total-money))
+                addMoney = eval(input("Please enter the money which is add :"))
+                print("Please put the money in the box")
+                print("Your money calculated")
+                newTotal = addMoney + money
+                if newTotal > total:
+                    print("Please take your change in the box  {}".format(newTotal-total))
+                elif newTotal == total:
+                    print("Process Sucesfull")
+                    break
+        if i == 6:
+            total = icecek6 * number
+            print("Your total debt: {} ₺ ".format(total))
+            print("Please put the money in the box")
+            print("Your money calculated")
+            money = eval(input("please enter the amount of money:"))
+            if money > total:
+                change = money - total
+                print("Please take the your change in the box: {}".format(change))
+            elif money == total:
+                print("process sucessfull")
+            else:
+                print("Your money not enough please put {} ₺".format(total-money))
+                addMoney = eval(input("Please enter the money which is add :"))
+                print("Please put the money in the box")
+                print("Your money calculated")
+                newTotal = addMoney + money
+                if newTotal > total:
+                    print("Please take your change in the box  {}".format(newTotal-total))
+                elif newTotal == total:
+                    print("Process Sucesfull")
+                    break
+        if i == 7:
+            total = icecek7 * number
+            print("Your total debt: {} ₺ ".format(total))
+            print("Please put the money in the box")
+            print("Your money calculated")
+            money = eval(input("please enter the amount of money:"))
+            if money > total:
+                change = money - total
+                print("Please take the your change in the box: {}".format(change))
+            elif money == total:
+                print("process sucessfull")
+            else:
+                print("Your money not enough please put {} ₺".format(total-money))
+                addMoney = eval(input("Please enter the money which is add :"))
+                print("Please put the money in the box")
+                print("Your money calculated")
+                newTotal = addMoney + money
+                if newTotal > total:
+                    print("Please take your change in the box  {}".format(newTotal-total))
+                elif newTotal == total:
+                    print("Process Sucesfull")
+                    break
+        if i == 8:
+            total = icecek8 * number
+            print("Your total debt: {} ₺ ".format(total))
+            print("Please put the money in the box")
+            print("Your money calculated")
+            money = eval(input("please enter the amount of money:"))
+            if money > total:
+                change = money - total
+                print("Please take the your change in the box: {}".format(change))
+            elif money == total:
+                print("process sucessfull")
+            else:
+                print("Your money not enough please put {} ₺".format(total-money))
+                addMoney = eval(input("Please enter the money which is add :"))
+                print("Please put the money in the box")
+                print("Your money calculated")
+                newTotal = addMoney + money
+                if newTotal > total:
+                    print("Please take your change in the box  {}".format(newTotal-total))
+                elif newTotal == total:
+                    print("Process Sucesfull")
+                    break
+        if i == 9:
+            total = icecek9 * number
+            print("Your total debt: {} ₺ ".format(total))
+            print("Please put the money in the box")
+            print("Your money calculated")
+            money = eval(input("please enter the amount of money:"))
+            if money > total:
+                change = money - total
+                print("Please take the your change in the box: {}".format(change))
+            elif money == total:
+                print("process sucessfull")
+            else:
+                print("Your money not enough please put {} ₺".format(total-money))
+                addMoney = eval(input("Please enter the money which is add :"))
+                print("Please put the money in the box")
+                print("Your money calculated")
+                newTotal = addMoney + money
+                if newTotal > total:
+                    print("Please take your change in the box  {}".format(newTotal-total))
+                elif newTotal == total:
+                    print("Process Sucesfull")
+                    break
+        if i == 10:
+            total = icecek1 * number
+            print("Your total debt: {} ₺ ".format(total))
+            print("Please put the money in the box")
+            print("Your money calculated")
+            money = eval(input("please enter the amount of money:"))
+            if money > total:
+                change = money - total
+                print("Please take the your change in the box: {}".format(change))
+            elif money == total:
+                print("process sucessfull")
+            else:
+                print("Your money not enough please put {} ₺".format(total-money))
+                addMoney = eval(input("Please enter the money which is add :"))
+                print("Please put the money in the box")
+                print("Your money calculated")
+                newTotal = addMoney + money
+                if newTotal > total:
+                    print("Please take your change in the box  {}".format(newTotal-total))
+                elif newTotal == total:
+                    print("Process Sucesfull")
+                    break
+        if i > 11:
+            print("invalid value Try again")
+        break
+    x = int(input("Do you want to anything else please press 1:"))
+    if x == 1:
+        continue
+    else:
+        break
+        
